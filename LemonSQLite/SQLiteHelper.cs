3a317585e994acd4a9a7caedd2a617aafc3c3dd6
huangmingji <229015504@qq.com>
2021-11-23 00:04:48 +0800

    实现 sqlitehelper

---
+++ LemonSQLite/SQLiteHelper.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.Data.SQLite;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace Lemon
+{
+    public class SQLiteHelper : IDisposable
+    {
+        private readonly SQLiteConnection _connection;
+        private readonly SQLiteCommand _command;
+
+        public SQLiteHelper(IConfiguration configuration)
+        {
+            // "Data Source=D:\\data.db3";
+            var connectionString = configuration.GetConnectionString("SQLite");
+            _connection = new SQLiteConnection(connectionString);
+            _connection.Open();
+            _command = new SQLiteCommand(_connection);
+        }
+
+        /// <summary>  
+        /// 创建SQLite数据库文件 
+        /// </summary>  
+        public void CreateDb()
+        {
+            _command.CommandText = "CREATE TABLE Test(id integer NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE)";
+            _command.ExecuteNonQuery();
+
+            _command.CommandText = "DROP TABLE Test";
+            _command.ExecuteNonQuery();
+        }
+
+        /// <summary>  
+        /// 对SQLite数据库执行增删改操作，返回受影响的行数。 
+        /// </summary>  
+        /// <param name="sql"> 要执行的增删改的SQL语句 </param> 
+        /// <param name="parameters"> 执行增删改语句所需要的参数，参数必须以它们在SQL语句中的顺序为准 </param> 
+        /// <returns></returns>  
+        public async Task<int> ExecuteNonQuery(string sql, SQLiteParameter[] parameters = null)
+        {
+            _command.CommandText = sql;
+            if (parameters != null && parameters.Length > 0)
+            {
+                _command.Parameters.AddRange(parameters);
+            }
+
+            return await _command.ExecuteNonQueryAsync();
+        }
+
+        /// <summary>  
+        /// 执行一个查询语句，返回一个关联的SQLiteDataReader实例 
+        /// </summary>  
+        /// <param name="sql"> 要执行的查询语句 </param> 
+        /// <param name="parameters"> 执行SQL查询语句所需要的参数，参数必须以它们在SQL语句中的顺序为准 </param> 
+        /// <returns></returns>  
+        public async Task<DbDataReader> ExecuteReader(string sql, SQLiteParameter[] parameters = null)
+        {
+            _command.CommandText = sql;
+            if (parameters != null && parameters.Length > 0)
+            {
+                _command.Parameters.AddRange(parameters);
+            }
+
+            return await _command.ExecuteReaderAsync(CommandBehavior.CloseConnection);
+        }
+
+        /// <summary>  
+        /// 执行一个查询语句，返回一个包含查询结果的DataTable 
+        /// </summary>  
+        /// <param name="sql"> 要执行的查询语句 </param> 
+        /// <param name="parameters"> 执行SQL查询语句所需要的参数，参数必须以它们在SQL语句中的顺序为准 </param> 
+        /// <returns></returns>  
+        public DataTable ExecuteDataTable(string sql, SQLiteParameter[] parameters = null)
+        {
+            _command.CommandText = sql;
+            if (parameters != null && parameters.Length > 0)
+            {
+                _command.Parameters.AddRange(parameters);
+            }
+
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(_command);
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+            return data;
+        }
+
+        /// <summary>  
+        /// 查询数据库中的所有数据类型信息 
+        /// </summary>  
+        /// <returns></returns>  
+        public DataTable GetSchema()
+        {
+            DataTable data = _connection.GetSchema("TABLES");
+            //foreach (DataColumn column in data.Columns) 
+            //{ 
+            //    Console.WriteLine(column.ColumnName); 
+            //} 
+            return data;
+        }
+
+        public void Dispose()
+        {
+            _command.Dispose();
+            _connection.Close();
+            _connection.Dispose();
+        }
+    }
+}
